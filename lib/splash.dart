ab3482cd1135e05fed3084c2ad5dc6526e31c3a2
Yassed Matta <yassed883@gmail.com>
2024-04-19 00:09:44 -0500

    Splash terminado

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:psycobienestar/login.dart';
 
 class Splash extends StatelessWidget {
-  const Splash({super.key});
+  const Splash({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFCFFFBB),
+      backgroundColor: const Color(0xFFCFFFBB),
       body: Center(
-        child: Image.asset('assets/img/Splash.png'),
+        child: Column(
+          children: [
+            const SizedBox(),
+            const Spacer(),
+            Image.asset('assets/img/Splash.png'),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Login()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+              ),
+              child: const Text('Entrar'),
+            ),
+          ],
+        ),
       ),
     );
   }
