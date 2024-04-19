3cf738613c689d207dd1252c1b15aff74d3492ac
Yassed Matta <yassed883@gmail.com>
2024-04-19 00:48:52 -0500

    Login terminado

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:psycobienestar/homePage.dart';
+import 'package:psycobienestar/register.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -7,28 +9,88 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        children: [
-          const Spacer(),
-          Image.asset('assets/img/Logo.png'),
-          const Spacer(),
-          const Text("Iniciar Sesión"),
-          const SizedBox(height: 20),
-          TextField(
-            style: const TextStyle(color: Color(0xFFCFFFBB)),
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white,
-              hintText: 'Email', // Texto de ayuda
-              hintStyle: const TextStyle(
-                  color: Colors.grey), // Estilo del texto de ayuda
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: const BorderSide(color: Colors.white),
+          child: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          children: [
+            const Spacer(),
+            Image.asset('assets/img/Logo.png'),
+            const Spacer(),
+            const Text(
+              'Iniciar sesión',
+              style: TextStyle(
+                fontSize: 24,
+                color: Color(0xFF1C5100),
               ),
             ),
-          )
-        ],
+            const SizedBox(height: 20),
+            const TextField(
+              style: TextStyle(color: Color(0xFF1C5100)),
+              decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(color: Color(0xFF1C5100)),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              style: TextStyle(color: Color(0xFF1C5100)),
+              decoration: InputDecoration(
+                hintText: 'Contraseña',
+                hintStyle: TextStyle(color: Color(0xFF1C5100)),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFF2B9800),
+              ),
+              child:
+                  const Text('Entrar', style: TextStyle(color: Colors.white)),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              '¿Olvidaste tu contraseña?',
+              style: TextStyle(
+                fontSize: 14,
+                color: Color(0xFF1C5100),
+              ),
+            ),
+            const Spacer(),
+            Row(children: [
+              Spacer(),
+              const Text("¿Aun no tienes cuenta?"),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Register()),
+                  );
+                },
+                child: const Text(
+                  'Registrate',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF1C5100),
+                  ),
+                ),
+              ),
+              Spacer(),
+            ]),
+            SizedBox(height: 5),
+            Container(
+              height: 1,
+              color: const Color(0xFF1C5100),
+            ),
+            const Spacer(),
+          ],
+        ),
       )),
     );
   }
